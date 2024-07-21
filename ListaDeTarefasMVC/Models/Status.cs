533c59effef13bb0162ad6b4f04bd47f595a182c
Leonardo Nunes <leonardonmattoslopes@hotmail.com>
2024-07-21 12:02:14 -0300

    Lista de tarefas com filtros de pesquisa

---
+++ ListaDeTarefasMVC/Models/Status.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+
+namespace ListaDeTarefasMVC.Models
+{
+    public class Status
+    {
+        public string StatusId { get; set; }
+        public string Nome { get; set; }
+    }
+}
